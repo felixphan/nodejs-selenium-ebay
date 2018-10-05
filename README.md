e9ca523960bc15e6d0498d6ec04f6a589ac107ad
vu.pt <vu.pt@temando.com>
2018-10-05 09:31:18 +0700

    Commit login function

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejs-selenium-ebay
 selenium for ebay
+
+# Manual
+1. Set driver folder in path
+2. Yarn
+3. index node.js (For ubuntu, chmod source folder is required `sudo chmod -R a+rwx .`)
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var webDriver = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
-var browser = new webDriver.Builder().withCapabilities(webDriver.Capabilities.chrome()).build();
 
-browser.get('https://www.google.com');
+var browser = new webDriver.Builder()
+  .withCapabilities(webDriver.Capabilities.chrome())
+  .build();
 
-var promise = browser.getTitle();
-
-promise.then(title => {
-    console.log(title);
+browser.get('https://ebay.com');
+const signInButton = browser.wait(
+  until.elementLocated(
+    By.xpath('/html/body/div[3]/div/header/div/ul[1]/li[1]/span/a')
+  ),
+  10000
+);
+signInButton.click().then(() => {
+  console.log('---Clicked Sign In Button---');
+});
+const userId = browser.wait(
+  until.elementLocated(By.xpath('//*[@id="userid"]')),
+  10000
+);
+userId.sendKeys('XXXXXXXX', Key.TAB).then(() => {
+  console.log('---Entered user---');
+});
+const pwd = browser.wait(
+  until.elementLocated(By.xpath('//*[@id="pass"]')),
+  10000
+);
+pwd.sendKeys('XXXXXXX', Key.RETURN).then(() => {
+  console.log('---Entered pwd---');
 });
